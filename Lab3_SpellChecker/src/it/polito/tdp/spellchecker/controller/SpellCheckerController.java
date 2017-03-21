667f314e4f1d4d29203b13268e483867da687dea
sassc <sassc@172.20.250.60>
2017-03-21 17:02:17 +0100

    Esercizio 1

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,14 +1,17 @@
 package it.polito.tdp.spellchecker.controller;
 
 import java.net.URL;
+import java.util.*;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.spellchecker.model.Dictionary;
+import it.polito.tdp.spellchecker.model.RichWord;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class SpellCheckerController {
 
@@ -16,9 +19,6 @@ public class SpellCheckerController {
 	public void setModel(Dictionary model) {
 		this.model = model;
 	}
-	
-	
-	
     @FXML
     private ResourceBundle resources;
 
@@ -29,38 +29,82 @@ public class SpellCheckerController {
     private ComboBox<String> cmbLanguage;
 
     @FXML
+    private TextArea txtInput;
+
+    @FXML
     private Button btnSpell;
 
     @FXML
-    private TextField txtError;
+    private TextArea txtOutput;
+
+    @FXML
+    private Label txtError;
 
     @FXML
     private Button btnClearText;
 
     @FXML
-    private TextField txtTime;
+    private Label txtTime;
 
     @FXML
     void doClearText(ActionEvent event) {
-
+    	txtInput.clear();
+    	txtOutput.clear();
+    	txtError.setVisible(false);
+    	txtTime.setVisible(false);
     }
 
     @FXML
     void doSpellCheck(ActionEvent event) {
-
+    	int numErrate=0;
+    	
+    	
+    	String lingua=cmbLanguage.getValue();
+    	if(lingua.equals("Italiano")){
+    		lingua="rsc/Italian.txt";
+    	}
+    	else
+    		lingua="rsc/English.txt";
+    	
+    	String paroleInserite=txtInput.getText();
+    	
+    	/*txtOutput.appendText(*/paroleInserite=paroleInserite.toLowerCase().replaceAll("[\\p{Punct}]", "");
+    	String array[]=paroleInserite.split(" ");
+    	List<String> listaInput=new ArrayList<String>(Arrays.asList(array));
+    	long t=System.nanoTime();
+    	model.loadDictionary(lingua);
+    	List<RichWord> listaOutput=new ArrayList<RichWord>(model.spellCheckText(listaInput));
+    	
+    	for(RichWord r: listaOutput){
+    		if(!r.isCorretta()){
+    			txtOutput.appendText(r.getParola()+"\n");
+    			numErrate++;
+    		}	
+    	}
+    	long t1=System.nanoTime();
+    	
+    	txtError.setText("Il testo contiene "+ numErrate+" errore/i.");
+    	txtTime.setText("Tempo necessario per la traduzione "+(t1-t)+ " nanosecondi.");
+    	txtError.setVisible(true);
+    	txtTime.setVisible(true);
     }
 
     @FXML
     void initialize() {
-        assert cmbLanguage != null : "fx:id=\"cmdLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert cmbLanguage != null : "fx:id=\"cmbLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert btnSpell != null : "fx:id=\"btnSpell\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtOutput != null : "fx:id=\"txtOutput\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert txtError != null : "fx:id=\"txtError\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
-        
+
         cmbLanguage.getItems().addAll("Italiano","English");
         if(cmbLanguage.getItems().size()>0)
         	cmbLanguage.setValue(cmbLanguage.getItems().get(0));
         
+        txtError.setVisible(false);
+    	txtTime.setVisible(false);
     }
 }
+
--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -1,5 +1,42 @@
 package it.polito.tdp.spellchecker.model;
 
-public class Dictionary {
+import java.io.*;
+import java.util.*;
 
+public class Dictionary {
+	
+	private List<String> dizionario;
+
+	public Dictionary() {
+		dizionario=new ArrayList<String>();
+	}
+	
+	public void loadDictionary(String language){
+		try {
+			FileReader fr = new FileReader(language);
+			BufferedReader br = new BufferedReader(fr);
+			String word;
+			while ((word = br.readLine()) != null) {
+				dizionario.add(word);
+			}
+			br.close();
+			} catch (IOException e){
+			System.out.println("Errore nella lettura del file");
+			}
+	}
+	
+	public List<RichWord> spellCheckText(List<String> inputTextList){
+		List<RichWord> listaParole=new LinkedList<RichWord>();
+		
+		for(String s: inputTextList){
+			RichWord r=new RichWord(s);
+			if(dizionario.contains(s)){
+				r.isCorrect();
+			}
+			listaParole.add(r);
+		}
+
+		return listaParole;
+	}
+	
 }
