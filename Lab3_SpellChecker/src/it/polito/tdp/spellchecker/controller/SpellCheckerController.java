5c020c7c717b98d6d3b85bddcde28952157f9d71
sassc <sassc@172.20.250.60>
2017-03-21 15:09:28 +0100

    Inizializzazione progetto

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,66 @@
 package it.polito.tdp.spellchecker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class SpellCheckerController {
+
+	Dictionary model;
+	public void setModel(Dictionary model) {
+		this.model = model;
+	}
 	
+	
+	
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> cmbLanguage;
+
+    @FXML
+    private Button btnSpell;
+
+    @FXML
+    private TextField txtError;
+
+    @FXML
+    private Button btnClearText;
+
+    @FXML
+    private TextField txtTime;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbLanguage != null : "fx:id=\"cmdLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnSpell != null : "fx:id=\"btnSpell\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtError != null : "fx:id=\"txtError\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        
+        cmbLanguage.getItems().addAll("Italiano","English");
+        if(cmbLanguage.getItems().size()>0)
+        	cmbLanguage.setValue(cmbLanguage.getItems().get(0));
+        
+    }
 }
